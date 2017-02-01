6f4e8248be86f86e7db7d59c76bdf3b6eea1e633
Emanuel Amiguinho <amiguinho_design@live.com.pt>
2017-02-01 23:06:19 +0000

    first commit; lets do it!

---
+++ Rustl/Rustl.Core.Interfaces/IoC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleInjector;
+
+namespace Rustl.Core.Interfaces
+{
+    public class IoC
+    {
+        private static Container _container;
+        public static Container Container { get { return _container ?? (_container = new Container()); } }
+
+    }
+}
